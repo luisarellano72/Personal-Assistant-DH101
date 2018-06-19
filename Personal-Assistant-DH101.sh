79ead7d7734e7824a306511afe383e52a1d5106f
Luis Arellano <vrellano@Luiss-MBP-3.hsd1.ca.comcast.net>
2018-06-18 21:20:29 -0700

    First commit

---
+++ Personal-Assistant-DH101.sh
@@ -0,0 +1,27 @@
+echo "Hello Luis! My name is Big Brother, your personal assisant! Remember, I am always watching! :)"
+
+date 
+
+cal
+
+echo "1. Use BASH to navigate the HFS"
+
+echo "2. pwd checks the current working directory"
+
+echo "3. cd allows you to change directories you are currently in"
+
+echo "4. ls -F lists the contents of what is inside each directory"
+
+echo "5. chmod +x makes programs like me executable"
+
+echo "6. ./someprogram.sh runs the mentioned program"
+
+echo "7. Use git pull to update current directory directly from a linked repository"
+
+echo "8. to clear the terminal of text,use ctrl-l"
+
+echo "9. Use mkdir to create a new folder"
+
+echo "10. Use touch to create new txt files"
+
+
